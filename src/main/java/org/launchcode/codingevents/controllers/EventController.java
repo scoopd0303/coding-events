ebcdd681ef84c235a2d1b07dd6f7f6756a129a9f
Scott McDonald <scottmcdonald@Scotts-MacBook-Air.local>
2021-06-01 12:27:24 -0500

    added images

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -8,16 +8,21 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 @Controller
 @RequestMapping("events")
 public class EventController {
 
-    private static List<String> events = new ArrayList<>();
+//    private static List<String> events = new ArrayList<>();
+    private static HashMap<String, String> events = new HashMap<>();
 
     @GetMapping
     public String displayAllEvents(Model model) {
+        events.put("Mario Party", "A party for some italian plumber.");
+        events.put("Molly's Birthday", "Come eat cake with an adorable pitbull.");
+        events.put("One-man pizza party", "my kind of friday night.");
         model.addAttribute("events", events);
         return "events/index";
     }
@@ -30,8 +35,8 @@ public class EventController {
 
     //lives at /events/create
     @PostMapping("create")
-    public String createEvent(@RequestParam String eventName) {
-        events.add(eventName);
+    public String createEvent(@RequestParam String eventName, @RequestParam String eventDesc) {
+        events.put(eventName, eventDesc);
         return "redirect:";
     }
 
